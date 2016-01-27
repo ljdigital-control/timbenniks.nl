01221c0c279502729b0b72811093f8744cdc131c
Tim Benniks <tim.benniks@akqa.com>
2016-01-28 00:29:05 +0100

    Added data.json for ease of use in blogposts

--- gulp/tasks/pages.js
+++ gulp/tasks/pages.js
@@ -1,14 +1,14 @@
 var gulp = require( 'gulp' ),
     config = require( '../config' ),
-    jade = require( 'gulp-jade' );
+    jade = require( 'gulp-jade' ),
+    data = require( '../../dev/assets/data.json' );
 
 gulp.task( 'pages', [ 'gitVersion' ], function(){
-
   return gulp
     .src( config.dev + 'pages/*.jade' )
     .pipe( jade({
       pretty: false,
-      locals: config
+      locals: { pages: data, config: config }
     } ) )
     .pipe( gulp.dest( config.dist ) );
 });
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -5,6 +5,7 @@ gulp.task( 'watch', function(){
   gulp.watch( [ config.dev + 'scripts/*.js', config.dev + 'scripts/**/*.js' ], [ 'lint', 'scripts' ] );
   gulp.watch( [ config.dev + 'styles/*.styl' ], [ 'styles' ] );
   gulp.watch( [ config.dev + 'assets/**/*' ], [ 'assets' ] );
-  gulp.watch( [ config.dev + 'pages/**/*.jade' ], [ 'pages' ] );
+  gulp.watch( [ config.dev + 'pages/**/*.jade', config.dev + 'assets/**/*.json' ], [ 'pages' ] );
   gulp.watch( [ config.dev + 'assets/**/*.jade' ], [ 'scripts' ] );
+
 });
