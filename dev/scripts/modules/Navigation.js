ab1d2ca5c785db0f27102699aa1c3c576ac18489
Tim Benniks <tim.benniks@akqa.com>
2017-01-16 14:18:41 +0100

    made navigation the same height as document

--- dev/scripts/modules/Navigation.js
+++ dev/scripts/modules/Navigation.js
@@ -4,6 +4,11 @@ class Navigation {
     this.nav = document.querySelector( '.main-nav' );
     this.innerNav = document.querySelector( '.navigation-body' );
 
+    let body = document.body,
+        html = document.documentElement;
+
+    this.docHeight = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight );
+
     if( !this.button ){
       return;
     }
@@ -19,14 +24,16 @@ class Navigation {
   }
 
   activate(){
-    document.body.style.overflow = 'hidden';
     this.button.classList.add( 'is-active' );
     this.nav.classList.add( 'is-active' );
     this.innerNav.classList.add( 'is-active' );
+    setTimeout( ()=>{
+      this.nav.style.height = `${this.docHeight}px`;
+    }, 300 );
   }
   
-  deactivate(){
-    document.body.style.overflow = 'auto';
+   deactivate(){
+    //document.body.style.overflow = 'auto';
     this.button.classList.remove( 'is-active' );
     this.nav.classList.remove( 'is-active' );
     this.innerNav.classList.remove( 'is-active' );
